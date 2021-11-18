cc4e9aafd4728760a7ccac2becea788596d1148b
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-18 09:48:28 -0300

    versão final

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,55 @@
-const express = require('express')
-require('dotenv').config()
-const postgres = require('@metamodules/postgres')()
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
-const app = express()
-const port = 4000
+require('dotenv').config();
+const postgres = require('@metamodules/postgres')();
 
-postgres.query(`CREATE TABLE IF NOT EXISTS clicks (
-  id BIGSERIAL PRIMARY KEY,
-  created_at TIMESTAMP DEFAULT NOW()
-)`)
+const app = express();
+const hostname = 'localhost';
+const port = process.env.PORT;
+require('./db');
 
-app.get('/api/count', (req, res) => {
-  postgres.query('SELECT count(*) AS count FROM clicks', (err, resp) => {
-    res.send({ count: resp.rows[0].count || 0 })
-  })
+const routesAdmin = require('./routes/Admins');
+const routesKid = require('./routes/Kids');
+const routesTask = require('./routes/Tasks');
+const routesTaskView = require('./routes/TaskView');
+const routesTaskReward = require('./routes/Rewards');
+const routesTaskResult = require('./routes/Results');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    "/kidPhoto",
+    express.static(path.resolve(__dirname, "./uploads/images/kidPhoto"))
+);
+app.use(
+    "/taskImage",
+    express.static(path.resolve(__dirname, "./uploads/images/taskImage"))
+);
+
+app.use(routesAdmin);
+app.use(routesKid);
+app.use(routesTask);
+app.use(routesTaskView);
+app.use(routesTaskReward);
+app.use(routesTaskResult);
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.json('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.get('/', (req, res) => {
+    return res.send("Server running!")
 })
 
-app.post('/api/count/increment', (req, res) => {
-  postgres.query('INSERT INTO clicks DEFAULT VALUES', (err, insert) => {
-    postgres.query('SELECT count(*) AS count FROM clicks', (err, resp) => {
-      res.send({ count: resp.rows[0].count || 0 })
-    })
-  })
-})
-
-app.listen(port, () => console.log(`Example backend API listening on port ${port}!`))
+app.listen(port);
