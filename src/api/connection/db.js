09ccc2ac95e66e606b78087c396a6e9edbb4a3c1
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-18 16:06:04 -0300

    db

--- src/api/connection/db.js
+++ src/api/connection/db.js
@@ -2,12 +2,11 @@ require("sequelize")
 
 module.exports = {
     dialect: 'mysql',
-    host: '34.151.232.43',
-    username: 'root',
-    password: '7797',
-    database: 'emotions',
+    host: process.env.CLEARDB_DATABASE_URL,
+    username: 'b8d4f404a89fc6',
+    password: '7c516d84',
+    database: process.env.CLEARDB_DATABASE,
     define: {
-        timezone: 'UTC-03:00',
         timestamps: true,
     },
 }
